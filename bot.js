26f747bcbdc757caa931f55c44fdaecafbfafd76
ibnusyawall <website.isas.24@gmail.com>
2020-09-28 15:01:34 +0700

    refactoring code

--- bot.js
+++ bot.js
@@ -1,37 +1,43 @@
-const Twit = require("twit");
+const Twiter = require("twit");
 const colors = require('colors/safe');
-const twit = new Twit(require("./config.js"));
-const mediaArtsSearch = { q: "mutual", count: 100, result_type: "recent" };
-const moment = require("moment")
+const twit   = new Twiter(require("./config.js"));
+const moment = require("moment");
 const ora = require('ora');
-const spinner = ora(' Twitter Bot Berjalan ..').start()
+
+const spinner = ora(' Twitter Bot Berjalan ..').start();
+
+const time = moment().format("HH:mm:ss");
+
+const mediaArtsSearch = { q: "mutual", count: 100, result_type: "recent" };
+
+const message = {
+    errorHastag: "There was an error with your hashtag search:",
+    errorRetweet: "Kamu sudah meretweet :)",
+    success: "Sukses NgeRetweet => \n || => ["
+}
 
 const retweetLatest = () => {
-  twit.get("search/tweets", mediaArtsSearch, (error, data) => {
-    if (!error) {
-      let retweetId = data.statuses[0].id_str;
-      twit.post("statuses/retweet/" + retweetId, {}, (error, response) => {
-        if (response) {
-        
-          console.log(`[ ${colors.blue(moment().format("HH:mm:ss"))} ]  ${colors.green(
-            "Sukses NgeRetweet => \n ||=>[")}${colors.yellow(data.statuses[0].text)} ]
-\n
-          `);
-        }
-        if (error) {
-          console.log(colors.red("Kamu sudah meretweet :)"));
-        }
-      });
-    }
-    else {
-      console.log("There was an error with your hashtag search:", error);
-    }
-  });
+  twit.get("search/tweets", mediaArtsSearch)
+      .then(data => {
+          let retweetId = data.statuses[0].id_str;
+          twit.post("statuses/retweet/" + retweetId, {})
+              .then(response => {
+                  console.log(`[ ${colors.blue(time)} ] ${colors.green(message.success)} ${colors.yellow(data.statuses[0].text)} ]\n`);
+              })
+              .catch(err => {
+                  console.log(colors.red(message.errorRetweet));
+              })
+      })
+      .catch(err => {
+          console.log(message.errorHastag, error);
+      })
 }
 
 retweetLatest();
+
 setInterval(retweetLatest, 10000);
+
 setTimeout(() => {
 	spinner.color = 'yellow';
-	spinner.text = ' Mendapatkan tweet ~\n';
+	spinner.text  = ' Mendapatkan tweet ~\n';
 }, 1000);
